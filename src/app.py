c66ff7d5ddc56c228c131874071de98ac5000cb1
sourabh-kaujalgi <Sourabh.kaujalgi@gmail.com>
2026-06-24 02:26:24 +0530

    Commiting yml file

--- src/app.py
+++ src/app.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     numbers = list(map(int, args))
     result = addition(*numbers)
-    print(f"The addition of {list(numbers)} are {result}")
+    print(f"The addition of {numbers} is {result}")
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,22 +1,17 @@
 from src import app
 
+def test_addition_with_positive_numbers():
+    result = app.addition(1, 2, 3)
+    assert result == 6
 
-def test_addition_with_two_numbers():
-    result = app.addition(2, 3)
-    assert result == 5
-
+def test_addition_with_zero_number():
+    result = app.addition()
+    assert result == 0
 
 def test_addition_with_negative_numbers():
     result = app.addition(-1, 3, -2)
     assert result == 0
 
-
 def test_addition_with_single_number():
     result = app.addition(-1)
     assert result == -1
-
-
-# Forcing this Test to Fail
-def test_addition_with_zero_number():
-    result = app.addition()
-    assert result == 1
